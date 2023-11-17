be939e02f427779ab1883c4103e5f5942a5f46b3
Samucahub <151155051+Samucahub@users.noreply.github.com>
2023-11-17 12:35:55 +0000

    Add files via upload

---
+++ Colours2followpagina.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Colours2Follow</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <style>
+    body {
+      font-family: 'Arial', sans-serif;
+      background-color: #f8f9fa;
+    }
+
+    header {
+      background-color: #66CDAA;
+      color: #fff;
+      padding: 1em;
+      text-align: center;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    nav {
+      background-color: #e9ecef;
+      padding: 1em;
+    }
+
+    nav a {
+      text-decoration: none;
+      color: #333;
+      margin: 0 15px;
+      font-weight: bold;
+    }
+
+    section {
+      padding: 40px 0;
+    }
+
+    footer {
+      background-color: #66CDAA;
+      color: #fff;
+      text-align: center;
+      padding: 1em 0;
+    }
+  </style>
+</head>
+<body>
+
+  <header>
+  <img src="GitHub.png" alt="Logo" />
+    <h1>Colours2Follow</h1>
+  </header>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="Colours2followpagina.php" style="font-weight: bold;">Início</a>
+    <a class="navbar-brand" href="Colours2followLoja.php">Loja</a>
+    <a class="navbar-brand" href="Colours2followFormulario.php">Formulário</a>
+    <a class="navbar-brand" href="Colours2followlocal.php">Local</a>
+  </nav>
+
+  <section id="home">
+    <div class="container">
+      <h2>Seção Inicial</h2>
+      <p>Bem-vindo à nossa página profissional.</p>
+    </div>
+  </section>
+
+  <section id="shop">
+    <div class="container">
+      <h2>Nossa Loja</h2>
+      <p>Explore nossa loja e descubra ótimos produtos.</p>
+    </div>
+  </section>
+
+  <section id="contact">
+    <div class="container">
+      <h2>Entre em Contato</h2>
+      <p>Entre em contato conosco para mais informações.</p>
+    </div>
+  </section>
+
+  <section id="address">
+    <div class="container">
+      <h2>Nossa Morada</h2>
+      <p>Nossa localização física.</p>
+    </div>
+  </section>
+
+  <footer>
+    <div class="container">
+      &copy; 2023 Colours2Follow. Todos os direitos reservados.
+    </div>
+  </footer>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>

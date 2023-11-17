1cfa2d46a25e4b5877a4f08c3d3d20f6e10da47d
Samucahub <151155051+Samucahub@users.noreply.github.com>
2023-11-17 16:09:23 +0000

    Update Colours2followLoja.php

--- Colours2followLoja.php
+++ Colours2followLoja.php
@@ -1,1 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>COLOURS2FOLLOW</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <style>
+    body {
+      font-family: 'Arial', sans-serif;
+      background-color: #f8f9fa;
+    }
 
+    header {
+      background-color: #FFA500;
+      color: #fff;
+      padding: 1em;
+      text-align: center;
+      align-items: center;
+    }
+    header img{
+      width: 50px;
+      height: auto;
+      margin-right: 10px;
+    }
+
+    header h1{
+      margin: 0;
+    }
+
+    nav {
+      background-color: #e9ecef;
+      padding: 1em;
+    }
+
+    nav a {
+      text-decoration: none;
+      color: #333;
+      margin: 0 15px;
+      font-weight: bold;
+    }
+
+    section {
+      padding: 40px 0;
+    }
+
+    footer {
+      background-color: #FFA500;
+      color: #fff;
+      text-align: center;
+      padding: 1em 0;
+    }
+  </style>
+</head>
+<body>
+
+  <header>
+    <h1>Loja</h1>
+  </header>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="Colours2followpagina.php">Início</a>
+    <a class="navbar-brand" href="Colours2followLoja.php" style="font-weight: bold;">Loja</a>
+    <a class="navbar-brand" href="Colours2followFormulario.php">Formulário</a>
+    <a class="navbar-brand" href="Colours2followlocal.php">Local</a>
+  </nav>
+
+  <section id="shop">
+    <div class="container">
+      <h2>Os nossos Produtos</h2>
+      
+      <br>
+      <br>
+      <br>
+      <br>
+
+  <footer>
+    <div class="container">
+      &copy; 2023 COLOURS2FOLLOW. Todos os direitos reservados.
+    </div>
+  </footer>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
